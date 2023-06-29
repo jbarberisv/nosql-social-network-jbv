ec46b6cf7b1fb3120ff9f3fe0a2eaa38c08fc35d
joaquin <joaquin@barberis.com>
2023-06-29 19:57:48 -0400

    server config

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+//importing mongoose
+const mongoose = require('mongoose');
+const app = express();
+//if not port in env, will use 3001
+const PORT = process.env.PORT || 3001;
+app.use(require('./routes'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-project-jbv', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true); // for log
+
+app.listen(PORT, () => console.log(`On localhost:${PORT}`));

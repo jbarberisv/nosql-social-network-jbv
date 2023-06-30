db3a378af526640e4f59508b296db1735e218956
joaquin <joaquin@barberis.com>
2023-06-29 21:11:51 -0400

    mongose configuration and user routes

---
+++ controllers/user-controller/index.js
@@ -0,0 +1,18 @@
+const { User } = require('../models');
+
+const userController = {
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }
+}
+
+module.exports = userController;
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [ this ]
+  }
+);
+
+
+const User = model('User', userSchema);
+module.exports = User;
